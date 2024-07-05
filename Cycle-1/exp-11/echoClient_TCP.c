3b24a7e34e26dcfab3c52ec1b79231f4725d6d4b
Alwin Mathew <113536301+AlwinMathew2003@users.noreply.github.com>
2024-07-06 00:31:45 +0530

    Update echoClient_TCP.c

--- Cycle-1/exp-11/echoClient_TCP.c
+++ Cycle-1/exp-11/echoClient_TCP.c
@@ -1,56 +1,46 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <sys/socket.h>
-#include <sys/types.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<arpa/inet.h>
+#include<unistd.h>
+#define port 5000
 
-#define PORT 4444
+void main(){
+	struct sockaddr_in serveraddr;
+	int clisocket;
+	
+	clisocket = socket(AF_INET,SOCK_STREAM,0);
+	if(clisocket>0)
+	{
+		printf("\nSocket is created");
+	}
 
-int main(){
-
-	int clientSocket, ret;
-	struct sockaddr_in serverAddr;
-	char buffer[1024];
-
-	clientSocket = socket(AF_INET, SOCK_STREAM, 0);
-	if(clientSocket < 0){
-		printf("[-]Error in connection.\n");
+	serveraddr.sin_family = AF_INET;
+	serveraddr.sin_port = htons(port);
+	serveraddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+	
+	int ret = connect(clisocket,(struct sockaddr *)&serveraddr,sizeof(serveraddr));
+	if(ret<0)
+	{
+		printf("Error in connection.\n");
 		exit(1);
 	}
-	printf("[+]Client Socket is created.\n");
-
-	memset(&serverAddr, '\0', sizeof(serverAddr));
-	serverAddr.sin_family = AF_INET;
-	serverAddr.sin_port = htons(PORT);
-	serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
-
-	ret = connect(clientSocket, (struct sockaddr*)&serverAddr, sizeof(serverAddr));
-	if(ret < 0){
-		printf("[-]Error in connection.\n");
-		exit(1);
-	}
-	printf("[+]Connected to Server.\n");
-
+	printf("\nConnected to server.");
+	char buffer[100];
 	while(1){
-		printf("Client: \t");
-		scanf("%s", &buffer[0]);
-		send(clientSocket, buffer, strlen(buffer), 0);
+		printf("\nEnter the string: ");
+		fgets(buffer,sizeof(buffer),stdin);
+		
+		send(clisocket,buffer,sizeof(buffer),0);
 
-		if(strcmp(buffer, ":exit") == 0){
-			close(clientSocket);
-			printf("[-]Disconnected from server.\n");
-			exit(1);
+		if(strcmp(buffer,"exit\n")==0)
+		{
+			break;
 		}
-
-		if(recv(clientSocket, buffer, 1024, 0) < 0){
-			printf("[-]Error in receiving data.\n");
-		}else{
-			printf("Server: \t%s\n", buffer);
-		}
+		recv(clisocket,buffer,sizeof(buffer),0);
+		printf("\nServer: %s",buffer);
+		
 	}
-
-	return 0;
+	close(clisocket);
+	
 }
